3515d52fdea9e7ec2557c81725c8ce029856de49
liamcoder <luyu.real@qq.com>
2020-11-29 16:28:11 +0800

    贪吃蛇-移动的贪吃蛇

---
+++ 2020/cls38贪吃蛇-移动的蛇.py
@@ -0,0 +1,75 @@
+import turtle as p
+
+p.ht()
+p.tracer(0)
+
+p.ht()
+snake = [
+    {"x": 10, "y": 0},
+    {"x": 10, "y": 30},
+    {"x": 10, "y": 60},
+]
+direction = {
+    "x": 0,
+    "y": 30
+}
+
+
+def changeRight():
+    global direction
+    direction["x"] = 30
+    direction["y"] = 0
+
+
+def changeLeft():
+    global direction
+    direction["x"] = -30
+    direction["y"] = 0
+
+
+def changeUp():
+    global direction
+    direction["x"] = 0
+    direction["y"] = 30
+
+
+def changeDown():
+    global direction
+    direction["x"] = 0
+    direction["y"] = -30
+
+
+def draw(x, y, color):
+    p.up()
+    p.goto(x, y)
+    p.dot(30, color)
+
+
+def move():
+    global snake
+    p.clear()
+    head = snake[-1].copy()
+    head['x'] += direction["x"]
+    head["y"] += direction["y"]
+    snake.append(head)
+    snake.pop(0)
+    for body in snake:
+        draw(body["x"], body["y"], "red")
+    p.ontimer(move, 200)
+
+
+for body in snake:
+    draw(body["x"], body["y"], "red")
+
+
+def start(x, y):
+    move()
+
+
+p.onkeypress(changeUp, "Up")
+p.onkeypress(changeDown, "Down")
+p.onkeypress(changeLeft, "Left")
+p.onkeypress(changeRight, "Right")
+p.onscreenclick(start)
+p.listen()
+p.done()
